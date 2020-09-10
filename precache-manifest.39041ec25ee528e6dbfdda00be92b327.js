432c588d573eb2699f5517724d9c02db2f848e65
sunnysingha911 <sunnysingha911@gmail.com>
2020-09-11 04:46:49 +0530

    Updates

---
+++ precache-manifest.39041ec25ee528e6dbfdda00be92b327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf00ed5bece3097beb846a775e6fef85",
+    "url": "/Corona_Tracker/index.html"
+  },
+  {
+    "revision": "ef8587308d6145e248d0",
+    "url": "/Corona_Tracker/static/css/main.22edab15.chunk.css"
+  },
+  {
+    "revision": "b5677563d362163e09a4",
+    "url": "/Corona_Tracker/static/js/2.44cf3600.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_Tracker/static/js/2.44cf3600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8587308d6145e248d0",
+    "url": "/Corona_Tracker/static/js/main.acd45151.chunk.js"
+  },
+  {
+    "revision": "91cac606595eaeb69998",
+    "url": "/Corona_Tracker/static/js/runtime-main.cd9effee.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona_Tracker/static/media/image.d7265326.png"
+  }
+]);
